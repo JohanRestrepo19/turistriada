a5ed09803f6440934d06f0f00db14537dba100e0
Brandddd <b.palacio1@utp.edu.co>
2023-04-08 22:05:52 -0500

    style: Redesigning of the navbar style for Turistriada app

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -3,17 +3,14 @@ import { ReactNode } from 'react';
 type IHeroOneButtonProps = {
   title: ReactNode;
   description: string;
-  button: ReactNode;
 };
 
 const HeroOneButton = (props: IHeroOneButtonProps) => (
   <header className="text-center">
-    <h1 className="text-5xl text-gray-900 font-bold whitespace-pre-line leading-hero">
+    <h1 className="text-6xl text-gray-900 font-bold leading-hero">
       {props.title}
     </h1>
     <div className="text-2xl mt-4 mb-16">{props.description}</div>
-
-    {props.button}
   </header>
 );
 
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import { Background } from '../background/Background';
-import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
@@ -9,37 +8,51 @@ import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-100">
-    <Section yPadding="py-6">
+    <Section yPadding="pt-7 pb-7">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
+          <Link href="/">
+            <a className="hover:text-primary-dark">Inicio</a>
           </Link>
         </li>
         <li>
           <Link href="/">
-            <a>Sign in</a>
+            <a className="hover:text-primary-dark">Beneficios</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className="hover:text-primary-dark">Planes</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className="hover:text-primary-dark">Contacto</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className="hover:text-primary-dark">Regístrate</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a className="hover:text-primary-dark">Iniciar Sesión</a>
           </Link>
         </li>
       </NavbarTwoColumns>
     </Section>
 
-    <Section yPadding="pt-20 pb-32">
+    <Section yPadding="pb-32">
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {
+              'Descubre Un Mundo Lleno De\nLugares Fantásticos Y\nDesconocidos De Nosotros Para\nTodos\n'
+            }
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <a>
-              <Button xl>Download Your Free Theme</Button>
-            </a>
-          </Link>
-        }
+        description="Una app hecha especialmente para el turismo en Pereira, Dosquebradas y Santa Rosa de Cabal"
       />
     </Section>
   </Background>
