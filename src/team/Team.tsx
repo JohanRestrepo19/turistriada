33361257c8f69bf729c095d9f99050482cac3569
Karen Grajales <karen.grajales@utp.edu.co>
2023-06-04 08:58:52 -0500

    feat: add links to github profiles.

--- src/team/Team.tsx
+++ src/team/Team.tsx
@@ -9,18 +9,21 @@ const Team = () => {
           imageSrc="/assets/images/members/brandon.png"
           name="Brandon Palacio"
           role="Software Developer"
+          github="https://github.com/Brandddd"
         />
 
         <TeamMember
           imageSrc="/assets/images/members/johan.png"
           name="Johan Restrepo"
           role="Software Developer"
+          github="https://github.com/JohanRestrepo19"
         />
 
         <TeamMember
           imageSrc="/assets/images/members/karen.png"
           name="Karen Grajales"
           role="Software Developer"
+          github="https://github.com/KarenG19"
         />
       </div>
     </Section>
